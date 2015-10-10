be7477a72f37ae7b5cc400e6724c336f5a5828ca
DarKnight <05.DarKnight.11@gmail.com>
2015-10-10 10:55:30 +0530

    main page up and working

--- workshop/mail_list/models.py
+++ workshop/mail_list/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.core.validators import RegexValidator
-
+from django.utils.encoding import smart_unicode
 # Create your models here.
 
 emailValidation = RegexValidator(r'^[0-9_ a-z A-Z \-\.]+(@iitbhu\.ac\.in|@itbhu\.ac\.in)*$')
-tenDigitContact = RegexValidator(r'^\d{10,10}$','Enter a Valid 10 digit number.', 'Invalid number')
+tenDigitContact = RegexValidator(r'^\d{10,10}$')
+
 
 class Participant(models.Model):
     LANGUAGE = {
@@ -21,9 +22,16 @@ class Participant(models.Model):
         ('wia', 'Windows App'),
         ('ioa', 'IOS App')
     }
-    name = models.CharField(max_length=50, blank=False)
-    email = models.EmailField(max_length=50, blank=False, validators=[emailValidation])
-    number = models.IntegerField(blank=False, validators=[tenDigitContact])
-    other_number = models.IntegerField(blank=True, null=True, validators=[tenDigitContact])
-    language = models.CharField(choices=LANGUAGE, max_length=10, blank=False)
-    platform = models.CharField(max_length=5, choices=PLATFORM, blank=False)
+    name = models.CharField(max_length=50, blank=False, error_messages={'invalid':"Please enter your name"})
+    email = models.EmailField(max_length=50, blank=False, validators=[emailValidation], unique=True,
+                              error_messages={'invalid':"Please enter official e-mail address"})
+    number = models.IntegerField(blank=False, validators=[tenDigitContact],
+                              error_messages={'invalid':"Please enter valid 10 digit number"})
+    other_number = models.IntegerField(blank=True, null=True, validators=[tenDigitContact],
+                              error_messages={'invalid':"Please enter valid 10 digit number"})
+    language = models.CharField(choices=LANGUAGE, max_length=10, blank=False,
+                                error_messages={'invalid':"Please choose a language"})
+    platform = models.CharField(max_length=5, choices=PLATFORM, blank=True)
+
+    def __unicode__(self):
+        return smart_unicode(self.name)+' : '+smart_unicode(self.number)+' : '+smart_unicode(self.email)
--- workshop/mail_list/views.py
+++ workshop/mail_list/views.py
@@ -1,36 +1,16 @@
 from django.shortcuts import render
-from django.views.generic.edit import FormView
-from django.core.urlresolvers import reverse
-
-# Create your views here.
-
+from django.utils import timezone
 from .forms import ContactForm
 
 
-class ContactFormView(FormView):
-    form_class = ContactForm
-    template_name = 'contact_form/contact_form.html'
-
-    def form_valid(self, form):
-        form.save()
-        return super(ContactFormView, self).form_valid(form)
-
-    def get_form_kwargs(self):
-        # ContactForm instances require instantiation with an
-        # HttpRequest.
-        kwargs = super(ContactFormView, self).get_form_kwargs()
-        kwargs.update({'request': self.request})
-        return kwargs
-
-    def get_success_url(self):
-        # This is in a method instead of the success_url attribute
-        # because doing it as an attribute would involve a
-        # module-level call to reverse(), creating a circular
-        # dependency between the URLConf (which imports this module)
-        # and this module (which would need to access the URLConf to
-        # make the reverse() call).
-        return reverse('contact_form_sent')
-
-
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return render(request, 'thanks.html')
+    else:
+        form = ContactForm()
+    return render(request, 'index.html', {'form': form})
